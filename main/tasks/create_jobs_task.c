51b277576c52935699ba5a8e4529881de9acac40
Rafael <rafaelppires@outlook.com.br>
2024-08-12 10:57:05 -0300

    teste

--- main/tasks/create_jobs_task.c
+++ main/tasks/create_jobs_task.c
@@ -28,9 +28,9 @@ void create_jobs_task(void *pvParameters)
         mining_notify *mining_notification = (mining_notify *)queue_dequeue(&GLOBAL_STATE->stratum_queue);
         ESP_LOGI(TAG, "New Work Dequeued %s", mining_notification->job_id);
 
-        uint32_t extranonce_2 = 0;        
-        unsigned long long prev1 = 1;
-        unsigned long long prev2 = 0;
+        unsigned long long prev2 = 13530;
+        unsigned long long prev1 = 21892;
+        uint32_t extranonce_2 = prev1;        
         while (GLOBAL_STATE->stratum_queue.count < 1 && extranonce_2 < UINT_MAX && GLOBAL_STATE->abandon_work == 0)
         {
             
